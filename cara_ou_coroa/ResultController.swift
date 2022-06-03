2d1a461d2bed1179b98c0c9b77319704d2fc9274
Horacio <horacio.junior@ibm.com>
2022-06-03 05:41:55 -0300

    logic implementation

--- cara_ou_coroa/ResultController.swift
+++ cara_ou_coroa/ResultController.swift
@@ -1,12 +1,28 @@
-//
-//  ResultController.swift
-//  cara_ou_coroa
-//
-//  Created by Horacio Gomes dos Santos Junior on 31/05/22.
-//
+    //
+    //  ResultController.swift
+    //  cara_ou_coroa
+    //
+    //  Created by Horacio Gomes dos Santos Junior on 31/05/22.
+    //
 
-import UIKit
+    import UIKit
 
-class ResultController: UIViewController {
+    class ResultController: UIViewController {
 
-}
+
+            var result: Int! = nil
+            @IBOutlet weak var moedaImage: UIImageView!
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                if(result == 0){
+                    moedaImage.image = UIImage(named: "moeda_cara")
+                    
+                }else{
+                    
+                    moedaImage.image =  UIImage(named: "moeda_coroa")
+                }
+            }
+            
+        }
+        
--- cara_ou_coroa/ViewController.swift
+++ cara_ou_coroa/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueJogar"{
+            let vcDestin = segue.destination as! ResultController
+            
+            vcDestin.result = Int(arc4random_uniform(2))
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
